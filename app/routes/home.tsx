28ea8697c6002a49eca8b907af7c7b35f7ecac22
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2025-06-15 09:28:04 +0530

    add html for cards in homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,27 @@
+import { Link } from "react-router";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Mockbuddy" },
+    { name: "description", content: "Welcome to MockBuddy!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <h1>Welcome to Mockbuddy</h1>
+      <div className="cards">
+        <div className="user-card">
+          <h2>Signup as User</h2>
+          <Link to={`/user-signup`}>Signup</Link>
+        </div>
+        <div className="interviewer-card">
+          <h2>Signup as Interviewer</h2>
+          <Link to={`/interviewer-signup`}>Signup</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
