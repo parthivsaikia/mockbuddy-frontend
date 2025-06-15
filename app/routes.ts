41e0f723d36398f13ae1496f162793fd7ee2d1ef
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2025-06-15 11:03:27 +0530

    register user-login route

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("/user-signup", "routes/user-signup.tsx"),
+  route("/user-login", "routes/user-login.tsx"),
 ] satisfies RouteConfig;
